f568ab83591b3c7d3a1a62a753ae1c18b90e373a
Ethan Roberts <theLameBrain@gmail.com>
2020-08-23 18:00:16 -0700

    CharonsLabyrinth_ReleaseCanidate.zip update

--- Assets/Scripts/GameEndController.cs
+++ Assets/Scripts/GameEndController.cs
@@ -31,4 +31,15 @@ public class GameEndController : MonoBehaviour
     {
         SceneManager.LoadScene("End-Lost");
     }
+
+    public void RestartGame()
+    {
+        GameManager.GAME.infir = false;
+        GameManager.GAME.serpt = false;
+        GameManager.GAME.eclyp = false;
+        GameManager.GAME.drake = false;
+        GameManager.GAME.paused = false;
+        GameManager.GAME.EnteredGardenFromMaze = true;
+        SceneManager.LoadScene("Outside");
+    }
 }
